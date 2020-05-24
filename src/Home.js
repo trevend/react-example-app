a42d232d1e6a7187c9bf6fee7a32d6e649069bfe
Alexander Schrot <alexander@axs-labs.com>
2020-05-24 19:17:57 +0200

    remove the newsletter section

--- src/Home.js
+++ src/Home.js
@@ -152,30 +152,6 @@ function Home() {
         </div>
       </div>
     </div>
-    {/* Newsletter */}
-    <div className="newsletter">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className="newsletter_border" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-8 offset-lg-2">
-            <div className="newsletter_content text-center">
-              <div className="newsletter_title">Subscribe to our newsletter</div>
-              <div className="newsletter_text"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a ultricies metus. Sed nec molestie eros</p></div>
-              <div className="newsletter_form_container">
-                <form action="#" id="newsletter_form" className="newsletter_form">
-                  <input type="email" className="newsletter_input" required="required" />
-                  <button className="newsletter_button trans_200"><span>Subscribe</span></button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
     {/* Footer */}
     <div className="footer_overlay" />
     <footer className="footer">
