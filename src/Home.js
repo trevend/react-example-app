51ed658cbde16efc0f90510d6c53710abfbe66be
Alexander Schrot <alexander@axs-labs.com>
2020-05-24 19:18:38 +0200

    add a product grid section

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from './components/Hero';
 import Navigation from './components/Navigation';
+import ProductGrid from './components/ProductGrid';
 
 function Home() {
   return <div className="super_container">
@@ -20,84 +21,7 @@ function Home() {
       <Hero />
     </div>
     {/* Products */}
-    <div className="products">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className="product_grid">
-              {/* Product */}
-              <div className="product">
-                <div className="product_image"><img src="images/product_1.jpg" alt="" /></div>
-                <div className="product_extra product_new"><a href="categories.html">New</a></div>
-                <div className="product_content">
-                  <div className="product_title"><a href="product.html">Smart Phone</a></div>
-                  <div className="product_price">$670</div>
-                </div>
-              </div>
-              {/* Product */}
-              <div className="product">
-                <div className="product_image"><img src="images/product_2.jpg" alt="" /></div>
-                <div className="product_extra product_sale"><a href="categories.html">Sale</a></div>
-                <div className="product_content">
-                  <div className="product_title"><a href="product.html">Smart Phone</a></div>
-                  <div className="product_price">$670</div>
-                </div>
-              </div>
-              {/* Product */}
-              <div className="product">
-                <div className="product_image"><img src="images/product_3.jpg" alt="" /></div>
-                <div className="product_content">
-                  <div className="product_title"><a href="product.html">Smart Phone</a></div>
-                  <div className="product_price">$670</div>
-                </div>
-              </div>
-              {/* Product */}
-              <div className="product">
-                <div className="product_image"><img src="images/product_4.jpg" alt="" /></div>
-                <div className="product_content">
-                  <div className="product_title"><a href="product.html">Smart Phone</a></div>
-                  <div className="product_price">$670</div>
-                </div>
-              </div>
-              {/* Product */}
-              <div className="product">
-                <div className="product_image"><img src="images/product_5.jpg" alt="" /></div>
-                <div className="product_content">
-                  <div className="product_title"><a href="product.html">Smart Phone</a></div>
-                  <div className="product_price">$670</div>
-                </div>
-              </div>
-              {/* Product */}
-              <div className="product">
-                <div className="product_image"><img src="images/product_6.jpg" alt="" /></div>
-                <div className="product_extra product_hot"><a href="categories.html">Hot</a></div>
-                <div className="product_content">
-                  <div className="product_title"><a href="product.html">Smart Phone</a></div>
-                  <div className="product_price">$670</div>
-                </div>
-              </div>
-              {/* Product */}
-              <div className="product">
-                <div className="product_image"><img src="images/product_7.jpg" alt="" /></div>
-                <div className="product_content">
-                  <div className="product_title"><a href="product.html">Smart Phone</a></div>
-                  <div className="product_price">$670</div>
-                </div>
-              </div>
-              {/* Product */}
-              <div className="product">
-                <div className="product_image"><img src="images/product_8.jpg" alt="" /></div>
-                <div className="product_extra product_sale"><a href="categories.html">Hot</a></div>
-                <div className="product_content">
-                  <div className="product_title"><a href="product.html">Smart Phone</a></div>
-                  <div className="product_price">$670</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <ProductGrid />
     {/* Ad */}
     <div className="avds_xl">
       <div className="container">
